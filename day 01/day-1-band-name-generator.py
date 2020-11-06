0f56becb45e63af3e114bf4136988ee5ff4e559e
Diana Chin <dchin@redlotusdesignz.com>
2020-11-05 21:37:57 -0500

    Day 1 Project
    Simple Band Name Generator that combines input functions and variables.

---
+++ day 01/day-1-band-name-generator.py
@@ -0,0 +1,7 @@
+# add a title
+print("Welcome to Betsy's Band Generator\n")
+# initialize the variable for input
+city = input("Which city are you from? ")
+snack = input("What is your favorite snack? ")
+# print the variables
+print("Your band name might be "+ city + ' ' + snack)
